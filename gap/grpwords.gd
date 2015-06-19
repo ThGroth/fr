303d288e64be090de821564fe844315e13ad1803
Thorsten <thorsten__groth@hotmail.com>
2015-06-19 16:01:53 +0200

    Some Comments addet.

--- gap/grpwords.gd
+++ gap/grpwords.gd
@@ -35,7 +35,18 @@ DeclareRepresentation("IsGrpWordStateRep",
 ####                         Constructors                              ####
 ####                                                                   ####
 ###########################################################################
-
+#GrpWord: Defines the Group word object given a List L and a Group G. The List can contain 
+#					positive numbers representing variables and group elements from the group G.
+#					The corresponding Group word is an element of F_\NN * G
+#
+#GrpWordHom: Defines the GroupWordHom object given a finite list of transitions. The i-th
+#						 Entry of the list corresponds to the image of i. So in this way group word homomorphisms
+#						 F_\NN * G \to F_\NN *G with finite support can be defined.
+#FRGrpWord: Defines the FRGrpWord object given a list L, a permutation p\in S_n and a Group G. 
+#						The list L should consist of groupwords over the Group G.
+#						The resultin object is an implementation of an element of (F_NN * G)^n \wr S_n
+#FRGrpWordUnknown: Defines a FRGroupWord object wich represents the unknowns of the corresponding group.
+#									 TODO 
 DeclareOperation("GrpWord", [IsList, IsGroup]);
 DeclareOperation("GrpWordHom",[IsList]);
 DeclareOperation("FRGrpWord",[IsList,IsPerm,IsGroup]);
